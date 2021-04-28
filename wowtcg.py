683d1eb4b073995cf26fd430f837f5f351d21169
Alexander.Ostertag <ostertag.alex@gmail.com>
2021-04-28 09:43:38 +0200

    renamed Class Teams to match added details to class

--- wowtcg.py
+++ wowtcg.py
@@ -6,35 +6,35 @@ players =['Alex', 'Joe', 'Jt','Spezi']
 
 
 
-class Teams:
-  def __init__(self, teams):
-    self.teams = teams
-    self.size = len(teams)
-    #check if Teams have equal number of players
-    noTeams=False
-    for i in teams:
-        if len(teams[0]) != len(i):
-            noTeams = True
-            break
-    if noTeams: print("Teamsizes do  not match")
+class Match:
+    def __init__(self, teams):
+        self.teams = teams
+        self.size = len(teams)
+        self.teamSize = 0
+        self.sTime = datetime.datetime.now()
+        #check if Teams have equal number of players
+        noTeams=False
+        for i in teams:
+            if len(teams[0]) != len(i):
+                noTeams = True
+                break
+        if noTeams: print("Teamsizes do  not match")
+        else: self.teamSize = len(teams[0])
+
+    def details(self):
+
+        for i in self.teams:
+            print('Team',self.teams.index(i)+1,end =' ')
+            for p in i:
+                print(p, end = ' ')
+            print()
+
+        print('Number of teams', self.size,'Teamsize',self.teamSize, 'StartTime', self.sTime )
 
 
-match = Teams([['Alex','Joe'],['Jt','Spezi'],['sjdk']])
-
-print(match.teams)
-print(match.size)
-
-# def gametype(teams):
-#     nrTeams = len(teams)
-#     print("Teams", nrTeams)
-#
-#     #check if Teams have equal number of players
-#     noTeams=False
-#     for i in teams:
-#         if len(teams[0]) != len(i):
-#             noTeams = True
-#             break
-#     if noTeams: print("Teamsizes do not match")
-#
-#
-# gametype([['Alex','Joe'],['Jt','Spezi']])
+match = Match([['Alex','Joe'],['Jt','Spezi']])
+match.details()
+# print(match.teams)
+# print(match.size)
+# print(match.teams[0])
+# print(match.sTime)
